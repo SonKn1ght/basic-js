1adb8826e0fde4b234ac3074a6b3a229bb3052e1
SonKn1ght <maximiilian321@gmail.com>
2020-09-25 13:51:58 +0300

    feat: ready 10/10

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {
+  repeatTimes: 0,
+  separator: '+',
+  addition: '',
+  additionRepeatTimes: 0,
+  additionSeparator: '|'
+}) {
+  let {repeatTimes = 1, separator = `+`, addition =``, additionRepeatTimes = 1, additionSeparator = '|'} = options;
+
+  let result = ``;
+  for (let i = 0; i < repeatTimes; i++) {
+    result += `${str}`;
+    let add = ``;
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      add += `${addition}${additionSeparator}`;
+    }
+    addResult = add.slice(0, add.length - additionSeparator.length)
+    result += `${addResult}${separator}`;
+  }
+  return result.slice(0, result.length - separator.length);
 };
-  
+
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,93 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this._flagReverse = flag;
+  }
+
+  getDict() {
+    let l = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`.split(``)
+    let square = [];
+    for (var i = 0; i < l.length; i++) {
+      square[i] = l.slice(i).concat(l.slice(0, i))
+    }
+    return square;
+  }
+
+  encrypt(message = null, key = null) {
+    if (message === null || key === null) {
+      throw new Error();
+    }
+
+    let alphabet = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    let dict = this.getDict();
+    let result = ``;
+    let j = 0;
+
+    if (key.length < message.length) {
+      let step = message.length - key.length;
+      for (let i = 0 ; i < step; i++) {
+        key+=key[i];
+      }
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+
+    message.split(``).map((cur, i) => {
+      if (alphabet.indexOf(cur) === -1) {
+        result += cur;
+        j -= 1;
+        return ;
+      }
+      i += j;
+
+      for (let stringDict of dict) {
+        if (stringDict[0] === key[(i)]) {
+          result += stringDict[alphabet.indexOf(cur)]
+        }
+      }
+    })
+
+    return (this._flagReverse ? result : result.split(``).reverse().join(``))
+  }
+
+  decrypt(message = null, key = null) {
+    if (message === null || key === null) {
+      throw new Error();
+    }
+    let alphabet = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    let dict = this.getDict();
+    let result = ``;
+    let j = ``;
+
+    if (key.length < message.length) {
+      let step = message.length - key.length;
+      for (let i = 0 ; i < step; i++) {
+        key+=key[i];
+      }
+    }
+    message = message.toUpperCase();
+    key = key.slice(0, message.length).toUpperCase();
+
+    key.split(``).map((cur, i) => {
+      if (alphabet.indexOf(message[i]) === -1) {
+        result += message[i];
+        j -= 1;
+        return ;
+      }
+      let k = i + j;
+
+      for (let stringDict of dict) {
+        if (stringDict[0] === key[(k)]) {
+          result += alphabet[stringDict.indexOf(message[i])]
+        }
+      }
+    })
+
+    return (this._flagReverse ? result : result.split(``).reverse().join(``))
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
